c199252b6c4dbcee276f3892af5c6c68b34547ac
Vikas Reddy <vikas85@terpmail.umd.edu>
2023-05-06 17:58:03 -0400

    first commit

---
+++ public/app.js
@@ -0,0 +1,20 @@
+const unitSelect = document.getElementById("unit");
+const heightCm = document.getElementById("height-cm");
+const heightFtIn = document.getElementById("height-ft-in");
+const rangeInput = document.getElementById('activity-range');
+const numberDisplay = document.getElementById('activity-number');
+
+unitSelect.addEventListener("change", function() {
+  if (unitSelect.value === "kg") {
+    heightCm.classList.remove("hide");
+    heightFtIn.classList.add("hide");
+  } else {
+    heightCm.classList.add("hide");
+    heightFtIn.classList.remove("hide");
+  }
+});
+
+rangeInput.addEventListener('input', function() {
+  const value = rangeInput.value;
+  numberDisplay.textContent = value;
+});
---
+++ public/data.js
@@ -0,0 +1,529 @@
+const Pancakes_Plain = {
+    query: 'Pancakes Plain',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy", "vegetarian"]
+  };
+
+  
+  const Pancakes_Chocolate_Chips = {
+    query: 'Pancakes Chocolate',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy", "vegetarian"]
+  };
+
+  const Scrambled_Egg = {
+    query: 'Scrambled Egg',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "vegetarian"]
+  };
+  
+  const Sandwich = {
+    query: 'Sandwich',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "soy"]
+  };
+  
+  const Grilled_Hot_Dog = {
+    query: 'Hot Dog',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["none"]
+  }
+  
+  const Onion_Marmalade_Sandwich = {
+    query: 'Onion Sandwich',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy"]
+  }
+
+  const Chorizo = {
+    query: 'Chorizo',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "soy"]
+  }
+  
+  const French_Toast = {
+    query: 'French Toast',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy", "vegetarian"]
+  }
+  
+  const English_Muffin = {
+    query: 'English Muffin',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "soy", "vegetarian"]
+  }
+  
+  const Greek_Yogurt = {
+    query: 'Greek Yogurt',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "vegetarian"]
+  }
+  
+  const Yogurt = {
+    query: 'Yogurt',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "vegetarian"]
+  }
+  
+  const Strawberry_Yogurt = {
+    query: 'Yogurt',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "vegetarian"]
+  }
+  
+  
+  const Biscuit = {
+    query: 'Biscuit',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "vegetarian"]
+  }
+  
+  const Mashed_Potatoes ={
+    query: 'Mashed Potatoes',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "vegetarian"]
+  }
+  
+  
+  const Cornbread ={
+    query: 'Cornbread',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "vegetarian"]
+  }
+  
+  const Croissant ={
+    query: 'Croissant',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "eggs", "soy", "vegetarian"]
+  }
+  
+  const Buffalo_Chicken_Pizza  = {
+    query: 'Buffalo wing',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy"] 
+  }
+  
+  const Cheese_Pizza  = {
+    query: 'Cheese Pizza',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["gluten", "dairy", "soy", "vegetarian", "eggs"] 
+  }
+  
+  const Margherita_Pizza  = {
+    query: 'Cheese Pizza',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["gluten", "dairy", "soy", "eggs"] 
+  }
+  const Pepperoni_Pizza  = {
+    query: 'Pepperoni Pizza',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["gluten", "dairy", "soy", "eggs"] 
+  }
+  
+  const White_Chicken = {
+    query: 'White Chicken',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy"] 
+  }
+  
+  const Pesto_Pasta = {
+    query: 'Pesto Pasta',
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "vegetarian"] 
+  }
+  
+  const Philly_Cheese_Steak_Pizza = {
+    query: "Pizza",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy"] 
+  }
+  
+  const Vegetable_Pizza = {
+    query: "Vegetable Pizza",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["gluten", "eggs", "dairy", "soy", "vegetarian"] 
+  } 
+  
+  const Grilled_Chicken_Burger = {
+    query: "Grilled Chicken Burger",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "soy"] 
+  }
+  
+  const BBQ_Chicken_Sandwich = {
+    query: "BBQ Sandwich",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "soy"] 
+  }
+  const Grilled_Cheese = {
+    query: "Grilled Cheese",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "soy", "vegetarian"] 
+  }
+  
+  const Steamed_Corn = {
+    query: "Corn",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "vegetarian"] 
+  } 
+  
+  const Mac_and_Cheese = {
+    query: "Mac and Cheese",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "vegetarian"] 
+  }
+  
+  const Italian_Meatballs_Pasta = {
+    query: "Italian Pasta",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten"] 
+  }
+  
+  const Garlic_Bread = {
+    query: "Garlic Bread",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "soy", "vegetarian"] 
+  }
+  
+  const Italian_Sausage_Pizza = {
+    query: "Mexican Pizza",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "soy", "eggs"] 
+  }
+  
+  const Pasta_Salad = {
+    query: "Pasta Salad",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy", "vegetarian"] 
+  }
+  
+  const Ice_Cream = {
+    query: "Ice Cream",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy", "vegetarian"] 
+  }
+  
+  const Brownie = {
+    query: "Brownie",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["none"] 
+  }
+  
+  const Donuts = {
+    query: "Donuts",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy", "vegetarian"] 
+  }
+  
+  const Waffle = {
+    query: "Waffle",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "vegetarian"] 
+  }
+  
+  const Cheeseburger = {
+    query: "Cheeseburger",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "soy"] 
+  }
+  
+  const Spaghetti = {
+    query: "Spaghetti",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "vegetarian"] 
+  }
+  
+  const Bow_Tie_Pasta = {
+    query: "Pasta",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "vegetarian"] 
+  }
+  
+  const Tomato_Soup = {
+    query: "Tomato Soup",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "vegetarian"] 
+  }
+  
+  const Potato_Soup = {
+    query: "Potato Soup",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten"] 
+  }
+  
+  const Cheese_Quesadilla = {
+    query: "Cheese Quesadilla",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "vegetarian"]
+  }
+  const Egg_Cheese_Wrap = {
+    query: "Egg burrito",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["none"]
+  }
+  
+  const Chicken_Cutlet = {
+    query: "Chicken",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten"] 
+  }
+  
+  const Pasta_with_Shrimp = {
+    query: "Pasta Shrimp",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten"] 
+  }
+  
+  const Pasta_Primavera = {
+    query: "Pasta Primavera",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "vegetarian"] 
+  }
+  
+  const Spiral_Pasta_with_Chicken = {
+    query: "Pasta, whole grain, with tomato-based sauce and poultry, restaurant",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["none"] 
+  }
+  
+  const Chilled_Pasta_Salad  = {
+    query: "Pasta Salad",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "vegetarian"] 
+  }
+  
+  const Butter_Chicken = {
+    query: "Butter Chicken",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy"] 
+  }
+  
+  const  Vegetable_Biryani = {
+    query: "Vegetable Biryani",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "vegetarian"] 
+  }
+  
+  const Tandoori_Chicken = {
+    query: "Chicken",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy"] 
+  }
+  
+  const Minestrone_Soup = {
+    query: "Minestrone Soup",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "vegetarian"] 
+  }
+  
+  const Bacon_Egg_Cheese = {
+    query: "Bacon Egg Cheese",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy"] 
+  }
+  
+  const Samosas = {
+    query: "Samosas",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "gluten", "vegetarian"] 
+  }
+  const Cheesecake = {
+    query: "Cheesecake",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    location: "Y",
+    time: "lunch",
+    dietary: ["dairy", "eggs", "gluten", "soy", "vegetarian"] 
+  }
+
+  module.exports = { Pancakes_Plain, Pancakes_Chocolate_Chips, Scrambled_Egg, Sandwich, Grilled_Hot_Dog, Onion_Marmalade_Sandwich, Chorizo, French_Toast, English_Muffin, Greek_Yogurt, Yogurt, Strawberry_Yogurt, Biscuit, Mashed_Potatoes, Cornbread, Croissant, Buffalo_Chicken_Pizza, Cheese_Pizza, Margherita_Pizza, Pepperoni_Pizza, White_Chicken, Pesto_Pasta, Philly_Cheese_Steak_Pizza, Vegetable_Pizza, Grilled_Chicken_Burger, BBQ_Chicken_Sandwich, Grilled_Cheese, Steamed_Corn, Mac_and_Cheese, Italian_Meatballs_Pasta, Garlic_Bread, Italian_Sausage_Pizza, Pasta_Salad, Ice_Cream, Brownie, Donuts, Waffle, Cheeseburger, Spaghetti, Bow_Tie_Pasta, Tomato_Soup, Potato_Soup, Cheese_Quesadilla, Egg_Cheese_Wrap, Chicken_Cutlet, Pasta_with_Shrimp, Pasta_Primavera, Spiral_Pasta_with_Chicken, Chilled_Pasta_Salad, Butter_Chicken, Vegetable_Biryani, Tandoori_Chicken, Minestrone_Soup, Bacon_Egg_Cheese, Samosas, Cheesecake}
+  
+  const North_Diner = {
+    breakfast: [Egg_Cheese_Wrap, French_Toast, Scrambled_Egg, Yogurt, Greek_Yogurt, Donuts, Greek_Yogurt, English_Muffin, Pancakes_Plain, Bacon_Egg_Cheese],
+  
+    lunch: [Chicken_Cutlet, Pasta_with_Shrimp, Pasta_Primavera, Spiral_Pasta_with_Chicken, Cheese_Pizza, Chilled_Pasta_Salad, Italian_Sausage_Pizza, Pepperoni_Pizza, Vegetable_Pizza, Butter_Chicken, Vegetable_Biryani, Tandoori_Chicken, Samosas, Tomato_Soup, Ice_Cream, Brownie, Minestrone_Soup],
+  
+    dinner: [Mac_and_Cheese, Cheese_Pizza, Chilled_Pasta_Salad, Italian_Sausage_Pizza, Pepperoni_Pizza, Vegetable_Pizza, Butter_Chicken, Tandoori_Chicken, Vegetable_Biryani, Samosas, Tomato_Soup, Minestrone_Soup, Ice_Cream, Brownie, Cheesecake]
+  }
+  
+  const South_Diner = {
+    breakfast: [Pancakes_Plain, Pancakes_Chocolate_Chips, Mashed_Potatoes, English_Muffin, Scrambled_Egg, Mac_and_Cheese, Steamed_Corn, Garlic_Bread, Yogurt, Greek_Yogurt, Strawberry_Yogurt, Croissant, Donuts, Waffle, Brownie, Ice_Cream],
+  
+    lunch: [Grilled_Chicken_Burger, BBQ_Chicken_Sandwich, Grilled_Cheese, Italian_Meatballs_Pasta, Italian_Sausage_Pizza, Pepperoni_Pizza, Vegetable_Pizza, Pasta_Salad, Yogurt, Greek_Yogurt, Strawberry_Yogurt, Ice_Cream],
+  
+    dinner: [Cheeseburger, Spaghetti, BBQ_Chicken_Sandwich, Grilled_Cheese, Steamed_Corn, Mac_and_Cheese,Cheese_Pizza, Pepperoni_Pizza, Italian_Sausage_Pizza, Vegetable_Pizza, Ice_Cream, Brownie, Donuts, Waffle, Tomato_Soup, Potato_Soup, Cheese_Quesadilla]
+  }
+  
+  const Yahentamitsi = {
+    breakfast: [Pancakes_Plain, Pancakes_Chocolate_Chips, Mashed_Potatoes,Scrambled_Egg, Chorizo, French_Toast, English_Muffin, Yogurt, Greek_Yogurt, Strawberry_Yogurt, Brownie, Ice_Cream],
+    
+    lunch: [Pancakes_Plain, Scrambled_Egg, Sandwich, Grilled_Hot_Dog, Onion_Marmalade_Sandwich, Chorizo, French_Toast, English_Muffin, Greek_Yogurt, Yogurt, Strawberry_Yogurt, Biscuit, Mashed_Potatoes, Cornbread, Croissant, Buffalo_Chicken_Pizza, Cheese_Pizza, Margherita_Pizza, Pepperoni_Pizza],
+    
+    dinner: [Grilled_Hot_Dog, Onion_Marmalade_Sandwich, White_Chicken, Pesto_Pasta, Philly_Cheese_Steak_Pizza, Vegetable_Pizza, Spaghetti]
+  };
+  module.exports = (North_Diner, South_Diner, Yahentamitsi)
